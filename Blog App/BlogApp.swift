55bfab36cb85684dfdf903077dcce0f69e12912e
ahmetbostanciklioglu <ahmetbostancikli@gmail.com>
2025-05-08 19:46:04 +0300

    Initial Commit

---
+++ Blog App/BlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blog_AppApp.swift
+//  Blog App
+//
+//  Created by Ahmet Bostanci on 8.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct BlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Blog App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Blog App
+//
+//  Created by Ahmet Bostanci on 8.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
